7f0939b339d57011aed9f7ff8aa641f6b5f0a911
stoppegp <stoppegp@users.noreply.github.com>
2016-01-19 14:15:18 +0100

    Comment style

--- functions.php
+++ functions.php
@@ -191,5 +191,46 @@ class pcamp_Nav_Menu extends Walker_Nav_Menu {
                 $output .= "$indent</ul></div>\n";
         }
 }
+function pcamp_comment($comment, $args, $depth) {
+	$GLOBALS['comment'] = $comment;
+	extract($args, EXTR_SKIP);
 
+	if ( 'div' == $args['style'] ) {
+		$tag = 'div';
+		$add_below = 'comment';
+	} else {
+		$tag = 'li';
+		$add_below = 'div-comment';
+	}
+?>
+	<<?php echo $tag ?> <?php comment_class( empty( $args['has_children'] ) ? '' : 'parent' ) ?> id="comment-<?php comment_ID() ?>">
+	<?php if ( 'div' != $args['style'] ) : ?>
+	<div id="div-comment-<?php comment_ID() ?>" class="comment-body">
+	<?php endif; ?>
+	<div class="comment-author vcard">
+	<?php if ($depth > 1) { ?>
+	<?php printf( __( '<cite class="fn">%s</cite> <span class="says">antwortete am</span>' ), get_comment_author_link() ); ?>
+	<?php } else { ?>
+	<?php printf( __( '<cite class="fn">%s</cite> <span class="says">schrieb am</span>' ), get_comment_author_link() ); ?>
+	<?php } ?>
+		<?php
+			/* translators: 1: date, 2: time */
+			printf( __('%1$s at %2$s'), get_comment_date(),  get_comment_time() ); ?>: <?php comment_reply_link( array_merge( $args, array( 'add_below' => $add_below, 'depth' => $depth, 'max_depth' => $args['max_depth'], 'reply_text' => "(Antworten)" ) ) ); ?><?php edit_comment_link( __( '(Edit)' ), '  ', '' );
+		?>
+	</div>
+	<?php if ( $comment->comment_approved == '0' ) : ?>
+		<em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.' ); ?></em>
+		<br />
+	<?php endif; ?>
+
+	<?php comment_text(); ?>
+
+	<!--<div class="reply">
+	<?php comment_reply_link( array_merge( $args, array( 'add_below' => $add_below, 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
+	</div>-->
+	<?php if ( 'div' != $args['style'] ) : ?>
+	</div>
+	<?php endif; ?>
+<?php
+}
 ?>
