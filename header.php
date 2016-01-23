b7f5099ce244484293df4893ec6bc0c983f3003c
stoppegp <stoppegp@users.noreply.github.com>
2016-01-23 03:00:41 +0100

    js html5 fix for ie8

--- header.php
+++ header.php
@@ -20,6 +20,16 @@
 		}
 	}).resize()
 		</script>
+<!--[if lt IE 9]>
+   <script>
+      document.createElement('header');
+      document.createElement('nav');
+      document.createElement('section');
+      document.createElement('article');
+      document.createElement('aside');
+      document.createElement('footer');
+   </script>
+<![endif]-->
     <?php wp_head(); ?>
 </head>
 <body>
