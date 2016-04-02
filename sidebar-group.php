0b402727ec31a050a4e90168ab8a97bf5cc5584b
stoppegp <stoppegp@users.noreply.github.com>
2016-04-02 23:38:49 +0200

    php open tag fix

--- sidebar-group.php
+++ sidebar-group.php
@@ -1,4 +1,4 @@
-<? global $groupslug; ?>
+<?php global $groupslug; ?>
 <div class="cright"><?php get_sidebar('pcamp'); ?><?php if ( !function_exists('dynamic_sidebar') ||
 			   !dynamic_sidebar('sidebar-pcamp-groups-'.$groupslug) ) : ?>
 	<?php endif; ?></div>
